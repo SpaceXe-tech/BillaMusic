350112058347e5c7fe8afe87652fd7f051600f5f
SpaceXe-tech <billaxe299@gmail.com>
2025-06-11 20:31:28 +0530

    Update resume.py

--- BillaMusic/plugins/admins/resume.py
+++ BillaMusic/plugins/admins/resume.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from BillaMusic import app
-from BillaMusic.core.call import BillaMusic
+from BillaMusic.core.call import Billa
 from BillaMusic.utils.database import is_music_playing, music_on
 from BillaMusic.utils.decorators import AdminRightsCheck
 from BillaMusic.utils.inline import close_markup
@@ -14,7 +14,7 @@ async def resume_com(cli, message: Message, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text("Stream is already resumed.")
     await music_on(chat_id)
-    await BillaMusic.resume_stream(chat_id)
+    await Billa.resume_stream(chat_id)
     await message.reply_text(
         "Stream resumed by {}".format(message.from_user.mention),
         reply_markup=close_markup(),
