58f768b9c3b8c238dfb41ae5cad72167854cc69f
SpaceXe-tech <billaxe299@gmail.com>
2025-06-11 20:30:32 +0530

    Update pause.py

--- BillaMusic/plugins/admins/pause.py
+++ BillaMusic/plugins/admins/pause.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from BillaMusic import app
-from BillaMusic.core.call import BillaMusic
+from BillaMusic.core.call import Billa
 from BillaMusic.utils.database import is_music_playing, music_off
 from BillaMusic.utils.decorators import AdminRightsCheck
 from BillaMusic.utils.inline import close_markup
@@ -14,7 +14,7 @@ async def pause_admin(cli, message: Message, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text("Song is already paused.")
     await music_off(chat_id)
-    await BillaMusic.pause_stream(chat_id)
+    await Billa.pause_stream(chat_id)
     await message.reply_text(
         "Song has been paused by {}".format(message.from_user.mention),
         reply_markup=close_markup(),
